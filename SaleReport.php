c547c56189926ae67c47a0b3196de21d1ea943ec
onur-yilmazz <info@onuryilmaz.web.tr>
2023-07-23 14:52:06 +0300

    Google Card and Bootstrap

--- SaleReport.php
+++ SaleReport.php
@@ -1,7 +1,9 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 class SaleReport extends \koolreport\KoolReport
+
 {
+    use \koolreport\clients\Bootstrap;
     protected function settings()
     {
         return array(
@@ -19,6 +21,9 @@ class SaleReport extends \koolreport\KoolReport
     protected function setup()
     {
         // "users" tablosundan veri çekmek için sorgu cümlesini belirtin
-        $this->src("automaker")->query("SELECT * FROM users")->pipe($this->dataStore("result"));
+        $this->src("automaker")
+        ->query("SELECT first_name as NAME, sum(price) as VALUE FROM users Group By id ORDER BY first_name LIMIT 10")
+        ->pipe($this->dataStore("result"));
     }
 }
+
--- SaleReport.view.php
+++ SaleReport.view.php
@@ -4,14 +4,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Users</title>
+
+
+    <style>
+       
+        .cssHeader
+        {
+            background-color:#e9ffe8;
+            font-family: "Times New Roman", Times, serif;
+            font-size:15px;
+        }
+        .cssItem
+        {
+            background-color:#fdffe8;
+            font-family: "Times New Roman", Times, serif;
+            font-size:15px;
+
+        }
+    </style>
+
+
 </head>
 <body>
-    <?php
-      
-        use \koolreport\widgets\koolphp\Table;
-        Table::create(array(
-            "dataSource" => $this->dataStore("result")
-        ));
-    ?>
+
+<?php
+      use \koolreport\widgets\koolphp\Table;
+      use \koolreport\widgets\google\AreaChart;
+      use \koolreport\widgets\google\BarChart;
+      use \koolreport\widgets\google\ColumnChart;
+      use \koolreport\widgets\google\TreeMap;
+      use \koolreport\widgets\google\PieChart;
+      use \koolreport\widgets\google\LineChart;
+      use \koolreport\widgets\google\ComboChart;
+      use \koolreport\widgets\google\DonutChart;
+      use \koolreport\widgets\google\Gauge;
+      use \koolreport\widgets\google\Sankey;
+      use \koolreport\widgets\google\ScatterChart;
+      use \koolreport\widgets\google\Timeline;
+      use \koolreport\widgets\google\GeoChart;
+      use \koolreport\widgets\google\Histogram;
+      use \koolreport\widgets\koolphp\Card;
+?>
+
+    <div class="container">
+        <div class="col-sm-12">
+            <?php
+                Card::create(array(
+                    "value"=>$this->src("automaker")->query("select count(id) from users"),
+                    "title"=>"Total Number of Members",
+                    "cssClass"=>array(
+                        "card"=>"bg-info",
+                        "title"=>"text-white",
+                        "value"=>"text-white"
+                    )
+                ));
+
+
+                Table::create(array(
+                    "dataSource" => $this->dataStore("result"),
+                    "cssClass"=>array(
+                        "th"=>"cssHeader",
+                        "tr"=>"cssItem"
+                    ),
+                    "paging"=>array(
+                        "pageSize"=>10,
+                        "pageIndex"=>0,
+                    ),
+                ));
+
+
+                BarChart::create(array(
+                    "dataSource" => $this->dataStore("result")
+                ));
+            ?>
+        </div>
+    </div>
+   
+
+       
+
+
 </body>
 </html>
